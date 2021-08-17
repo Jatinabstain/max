4fba86e209f44f23898789ecec76a7435e17844f
Jatin Verma <54092377+Jatinabstain@users.noreply.github.com>
2021-08-17 16:35:20 +0530

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -5,8 +5,6 @@ import About from '../public/assets/images/about.png'
 import Link from 'next/link'
 import Carousel from 'react-bootstrap/Carousel';
 
-// import 'bootstrap/dist/js/bootstrap'
-// import 'jquery/dist/jquery'
 export const Main = () => {
     return (
         <div className="layout">
@@ -16,8 +14,6 @@ export const Main = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className="wrap">
-
-                {/* banner */}
                 <Carousel>
                     <Carousel.Item >
                         <Image src={Banner} className="img-fluid" alt="banner" />
@@ -44,7 +40,7 @@ export const Main = () => {
                                 <div className="about_content">
                                     <h4>Welcome to <span>Max Movers</span></h4>
                                     <p> Now were up in the big leagues getting our turn at bat. And when the odds are against him and their dangers work to do.</p>
-                                    <p className="text-dark">Some Our <span className="sincerity"> Sincerity,</span></p>
+                                    <p className="text-dark">Some Our <span className="sincerity"> Sincerity</span></p>
                                     <ul className="list-unstyled about_list">
                                         <li><i className="fas fa-check"></i> Our Works Are Doing Very Fast.</li>
                                         <li><i className="fas fa-check"></i> Why do we always come here I guess well never know.</li>
