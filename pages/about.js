111827cb8df6b0046c807ebd751449e47382bf65
Jatin Verma <54092377+Jatinabstain@users.noreply.github.com>
2021-08-18 13:24:31 +0530

    2

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Accordion, Card } from "react-bootstrap";
+import team_user from '../public/assets/images/team-1.jpg'
+import team_user1 from '../public/assets/images/team-2.jpg'
+import team_user2 from '../public/assets/images/team-4.jpg'
+import team_user3 from '../public/assets/images/team-3.jpg'
 
 export const About = () => {
     return (
@@ -20,8 +24,123 @@ export const About = () => {
                         </p>
                     </div>
                 </section>
-                <div className="container">
-                <Accordion className="mt-5 mb-5" defaultActiveKey="0">
+                <section className="about_team">
+                    <div className="container">
+                        <div className="team_heading">
+                            <h3>Young <span>Max Movers</span> Team</h3>
+                        </div>
+                        <div className="team_lists">
+                            <div className="row">
+                                <div className="col-md-3">
+                                    <div className="team_box">
+                                        <Image src={team_user} className="img-fluid" alt="team" />
+                                        <div className="team_body">
+                                            <ul>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-facebook-f"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-twitter"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-instagram"></i></a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                            <h5>Simon Smith</h5>
+                                            <p>Manager</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="team_box">
+                                        <Image src={team_user1} className="img-fluid" alt="team" />
+                                        <div className="team_body">
+                                            <ul>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-facebook-f"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-twitter"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-instagram"></i></a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                            <h5>JOHN DEO</h5>
+                                            <p>Product Manager</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="team_box">
+                                        <Image src={team_user2} className="img-fluid" alt="team" />
+                                        <div className="team_body">
+                                            <ul>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-facebook-f"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-twitter"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-instagram"></i></a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                            <h5>LARA LEE</h5>
+                                            <p>Finance Manager</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="team_box">
+                                        <Image src={team_user3} className="img-fluid" alt="team" />
+                                        <div className="team_body">
+                                            <ul>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-facebook-f"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-twitter"></i></a>
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link href="#!">
+                                                        <a><i className="fab fa-instagram"></i></a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                            <h5>WHITE CLARK</h5>
+                                            <p>HR Manager</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="container d-none">
+                    <Accordion className="mt-5 mb-5" defaultActiveKey="0">
                         <Card>
                             <Accordion.Toggle as={Card.Header} eventKey="0" className="accordion_card">
                                 TAB 1
